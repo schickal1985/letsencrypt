000a390a03a50a73f648b5b976fab52df02e9aa6
schickal <schickal1985@gmail.com>
2026-03-06 22:24:39 +0100

    Refactor to Background Jobs Architecture (Lösung B)

--- dns_wait_hook.sh
+++ dns_wait_hook.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-# Version: 0.9
-exec > /dev/tty 2>&1
+# Version: 1.0 (Parallel Background Jobs)
+# Wird von den unsichtbaren Background-Certbots aufgerufen.
+# Gibt keinen Text an den User aus, sondern loggt nur den Key.
 
-echo -e "\e[1;33m>>> SCHLÜSSEL GEFORDERT <<<\e[0m"
-echo -e "Bitte folgenden TXT-Eintrag im DNS hinterlegen:"
-echo -e "Host: \e[1;36m_acme-challenge.$CERTBOT_DOMAIN\e[0m"
-echo -e "Wert: \e[1;36m$CERTBOT_VALIDATION\e[0m"
-echo -e "--------------------------------------------------------\n"
+echo "$CERTBOT_DOMAIN $CERTBOT_VALIDATION" >> /tmp/certbot_check_list.txt
 
-# Wir sichern den Eintrag für unser Checker-Skript im Puffer
-echo "_acme-challenge.$CERTBOT_DOMAIN $CERTBOT_VALIDATION" >> /tmp/certbot_check_list.txt
-
-# WICHTIG: Kein Sleep oder Warten mehr! 
-# Wir beenden diesen Hook sofort, damit Certbot beim 2., 3., 4. Key 
-# sofort weitermacht und dir alle Keys am Stück listet!
+# Beende Hook sofort, damit Certbot weiterläuft bis "Press Enter to Continue"
 exit 0
